2696d51f53b4a9327080f933fed71b1bcbf131d2
cgatto <christian.gatto@txtgroup.com>
2023-10-10 12:15:57 +0200

    final commit

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
-export default axios.create({
-	// baseURL: 'http://192.168.1.23:8080',
-	baseURL: 'http://100.64.81.77:6969/',
+const api = axios.create({
+	baseURL: 'http://100.69.119.20:6969/gpt',
 	headers: { 
-		"ngrok-skip-browser-warning": "true",
+		"ngrok-skip-browser-warning" : "true",
 		"Access-Control-Allow-Origin" : "*",
 		"Access-Control-Allow-Headers" : "*",
 		"Access-Control-Allow-Methods" : "*",
 		"Content-Type" : "application/json",
 	}
 })
+
+const updateBaseUrl = (newBaseUrl) => {
+	// debugger
+	// console.log(api.defaults)
+	api.defaults.baseURL = "http://100.69.119.20:6969/" + newBaseUrl;
+	// console.log(api.defaults)
+};
+
+export default api;
+export { updateBaseUrl };
--- src/components/Dragger/Dragger.jsx
+++ src/components/Dragger/Dragger.jsx
@@ -1,114 +1,143 @@
 import React, { useState, useEffect } from "react";
-import { Button, Card, Col, Toast } from "react-bootstrap"
-import simulation_files from "../../files.json"
+import { Button, Card, Col, Toast } from "react-bootstrap";
+import simulation_files from "../../files.json";
 import "./Dragger.scss";
-import { faCloudArrowUp, faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCloudArrowUp,
+	faFolderOpen,
+	faLock,
+	faLockOpen,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDropzone } from "react-dropzone";
-import api from '../../API/axiosConfig';
+import api, { updateBaseUrl } from "../../API/axiosConfig";
 
 const Dragger = (props) => {
-	const [error, setError] = useState(false)
-	const [filesInfo, setFilesInfo] = useState([])
-	const [showToast, setShowToast] = useState(false)
-	const [multipleFiles, setMultipleFiles] = useState([])
-	
-	const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone({
-		accept: 
-		{
-			'application/pdf': ['.pdf'],
-		}
-	});
+	const [error, setError] = useState(false);
+	const [filesInfo, setFilesInfo] = useState([]);
+	const [showToast, setShowToast] = useState(false);
+	const [multipleFiles, setMultipleFiles] = useState([]);
+	const [llm, setLlm] = useState("gpt");
 
-	const sendFilesToBackend = (fileList) => {
-		// debugger
-		const formData = new FormData();
-		fileList.forEach((file) => {
-			formData.append('files', file); // Append each file to the FormData object
+	const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
+		useDropzone({
+			accept: {
+				"application/pdf": [".pdf"],
+			},
 		});
 
-		props.setLoading(true)
+	const sendFilesToBackend = () => {
+		// debugger
+
+		api.post("/delete_all");
+
+		const formData = new FormData();
+		filesInfo.forEach((file) => {
+			formData.append("files", file); // Append each file to the FormData object
+		});
+
+		props.setLoading(true);
 		api.post("/upload", formData, {
 			headers: {
-				"Content-Type" : 'multipart/form-data'
-			}
+				"Content-Type": "multipart/form-data",
+			},
 		})
-		.then((response) => {
-			console.warn(response.status)
-			if(response && response.status < 200 && response.status >= 300) {
-				setError(true)
-				throw new Error("Bad network response")
-			}
-			else {
-				setError(false)
-				props.setLoading(true)
-				return response.data
-			}
-		})
-		.then(data => {
-			console.log("data received after upload: ", data)
-			props.setLoading(false)
-			setFilesInfo(prevFiles => [...prevFiles, ...data.uploaded_files])
-		})
-		.catch(error => console.error("Error in uploading file: ", error))
-	}
-	
-	useEffect(() => {
+			.then((response) => {
+				console.warn(response.status);
+				if (
+					response &&
+					response.status < 200 &&
+					response.status >= 300
+				) {
+					setError(true);
+					throw new Error("Bad network response");
+				} else {
+					setError(false);
+					props.setLoading(true);
+					return response.data;
+				}
+			})
+			.then((data) => {
+				console.log("data received after upload: ", data);
+				props.setLoading(false);
+				setFilesInfo((prevFiles) => [
+					...prevFiles,
+					...data.uploaded_files,
+				]);
+				props.setPage(1);
+			})
+			.catch((error) =>
+				console.error("Error in uploading file: ", error)
+			);
+	};
+
+	const removeFile = (filenameToRemove) => {
 		// debugger
-		// filter if new files uploaded are already present
-		const multiple = filesInfo.filter(filePresent => acceptedFiles.some(newFile => newFile.name === filePresent.filename))
-		if(multiple) {
-			setMultipleFiles(multiple.map(f => f.filename))
-		}
-		const droppedFiles = Array.from(acceptedFiles.filter(newFile => !filesInfo.some(filePresent => filePresent.filename === newFile.name)));
-		if(droppedFiles.length > 0) {
-			// DONE: MAKE A CALL TO BACKEND TO GET DESCRIPTION FOR EACH FILE (i send files array and
-			// get a response with a descriptions array)
-			sendFilesToBackend(droppedFiles)
-		}
-	}, [acceptedFiles])
-
-	const removeFileFromBackend = (filename) => {
-		const payload = {
-			filename: filename
-		}
-
-		props.setLoading(true)
-		api.post("/delete", JSON.stringify(payload))
-		.then((response) => {
-			// console.warn(response.status)
-			if(response && response.status < 200 && response.status >= 300) {
-				setError(true)
-				throw new Error("Bad network response")
-			}
-			else {
-				setError(false)
-				return response.data
-			}
-		})
-		.then(data => {
-			// console.log("data removed: ", data)
-			props.setLoading(false)
-			setFilesInfo(filesInfo.filter((f) => f.filename !== filename))
-		})
-		.catch(error => console.error("Error in deleting file: ", error))
-	}
+		var newFileInfo = filesInfo.filter(
+			(file) => file.name !== filenameToRemove
+		);
+		setFilesInfo(newFileInfo);
+	};
 
 	useEffect(() => {
-		if(multipleFiles.length > 0)
-			setShowToast(true)
-	}, [multipleFiles])
-	
-	
+		if (acceptedFiles.length > 0) {
+			// debugger
+			// filter if new files uploaded are already present
+			const multiple = filesInfo.filter((filePresent) =>
+				acceptedFiles.some(
+					(newFile) => newFile.name === filePresent.name
+				)
+			);
+			if (multiple) {
+				setMultipleFiles(multiple.map((f) => f.name));
+			}
+			setFilesInfo((prevFiles) => [
+				...prevFiles,
+				...acceptedFiles.filter(
+					(newFile) =>
+						!prevFiles.some(
+							(filePresent) => filePresent.name === newFile.name
+						)
+				),
+			]);
+		}
+	}, [acceptedFiles]);
+
+	useEffect(() => {
+		if (multipleFiles.length > 0) setShowToast(true);
+	}, [multipleFiles]);
+
+	const switchLlm = () => {
+		llm === "llama" ? setLlm("gpt") : setLlm("llama");
+	};
+
+	useEffect(() => {
+		updateBaseUrl(llm);
+	}, [llm]);
 
 	return (
 		<>
-			<Col xs={11} md={6} className={isDragActive ? "dragger dragging" : "dragger"} {...getRootProps()}>
-				<FontAwesomeIcon className="icon" icon={isDragActive ? faFolderOpen : faCloudArrowUp} />
+			<div className="model-selector-container">
+				<Button className="llm-btn" onClick={() => switchLlm()}>
+					<FontAwesomeIcon
+						icon={llm === "gpt" ? faLockOpen : faLock}
+					/>
+				</Button>
+			</div>
+			<Col
+				xs={11}
+				md={6}
+				className={isDragActive ? "dragger dragging" : "dragger"}
+				{...getRootProps()}
+			>
+				<FontAwesomeIcon
+					className="icon"
+					icon={isDragActive ? faFolderOpen : faCloudArrowUp}
+				/>
 				<input {...getInputProps()} />
 				{isDragActive ? (
 					<h4 className="m-0">Drop the files here</h4>
-					) : (
+				) : (
 					<>
 						<h3 className="m-0">Drag&Drop some files here</h3>
 						<h6 className="m-0">or</h6>
@@ -116,38 +145,49 @@ const Dragger = (props) => {
 					</>
 				)}
 			</Col>
-			{filesInfo?.length > 0 &&
+			{filesInfo?.length > 0 && (
 				<Col xs={11} md={6} className="pdf-cards">
 					{filesInfo.map((file, index) => (
 						<Card key={index}>
 							<Card.Header>
-								<span>{file.filename}</span>
+								<span>{file.name}</span>
 							</Card.Header>
 							<Card.Body>
-								<Card.Text className="slide-right">{file.description}</Card.Text>
+								<Card.Text className="slide-right">
+									Provola affumicata
+								</Card.Text>
 							</Card.Body>
 							<Card.Footer>
-								<Button className="rm-btn" onClick={() => removeFileFromBackend(file.filename)}>
+								<Button
+									className="rm-btn"
+									onClick={() => removeFile(file.name)}
+								>
 									Remove
 								</Button>
 							</Card.Footer>
 						</Card>
 					))}
 				</Col>
-			}
-			{error &&
-				<div className="error-msg">
-					Non va un cazzo qua internet
-				</div>
-			}
+			)}
+			{error && (
+				<div className="error-msg">Error during upload</div>
+			)}
 			<Button
 				className="starter-btn"
-				onClick={() => props.setPage(1)}
-				disabled={filesInfo.length <= 0}>
+				onClick={() => sendFilesToBackend()}
+				disabled={filesInfo.length <= 0}
+			>
 				Start Analysis
 			</Button>
-			<Toast show={showToast} onClose={() => setShowToast(false)} delay={5000} autohide>
-				<Toast.Body><i>{multipleFiles.join(', ')}</i> already uploaded!</Toast.Body>
+			<Toast
+				show={showToast}
+				onClose={() => setShowToast(false)}
+				delay={5000}
+				autohide
+			>
+				<Toast.Body>
+					<i>{multipleFiles.join(", ")}</i> already uploaded!
+				</Toast.Body>
 			</Toast>
 		</>
 	);
